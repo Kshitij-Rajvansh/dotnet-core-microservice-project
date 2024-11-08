fbfe727a032961cec1a331116a153cff499fb907
newfangledCoder <kshitij.raj@hotmail.com>
2024-11-08 17:23:49 +0530

    adding search service

---
+++ src/SearchService/RequestHelpers/SearchParams.cs
@@ -0,0 +1,15 @@
+using System;
+using MongoDB.Entities;
+
+namespace SearchService.RequestHelpers;
+
+public class SearchParams
+{
+    public string SearchTerm { get; set; }
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 4;
+    public string OrderBy { get; set; }
+    public string FilterBy { get; set; }
+}
